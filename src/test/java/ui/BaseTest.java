cfa49d93318988b9cc60a1a537fd73cb124f2a6f
Polina Bogdanovich <yapolli10@gmail.com>
2025-10-06 20:10:56 +0300

    add BaseTest.java

---
+++ src/test/java/ui/BaseTest.java
@@ -0,0 +1,24 @@
+package ui;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
+}
