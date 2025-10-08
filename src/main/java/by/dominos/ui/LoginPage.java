870f365c5af95095cf679c4974da2c5015578aa4
Polina Bogdanovich <yapolli10@gmail.com>
2025-10-08 19:25:08 +0300

    refactor code

--- src/main/java/by/dominos/ui/LoginPage.java
+++ src/main/java/by/dominos/ui/LoginPage.java
@@ -6,6 +6,7 @@ public class LoginPage {
     private final String BUTTON_FOR_CLIENTS = "//button[@class='red-button auth-modal__button-active']";
     private final String INPUT_PHONE_NUMBER = "//input[@type='tel']";
     private final String BUTTON_JOIN = "//button[@class='red-button auth-modal__submit-button']";
+    private final String INPUT_CODE = "//input[@class='input__component  auth-modal__input']";
 
     public void clickForClients() {
         WebDriverSingleton.clickElement(BUTTON_FOR_CLIENTS);
@@ -17,5 +18,9 @@ public class LoginPage {
 
     public void clickJoin() {
         WebDriverSingleton.clickElement(BUTTON_JOIN);
+    }
+
+    public void sendKeysCode(String code) {
+        WebDriverSingleton.sendKeys(INPUT_CODE, code);
     }
 }
