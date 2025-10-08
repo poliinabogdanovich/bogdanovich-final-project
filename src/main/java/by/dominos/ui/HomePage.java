6abdb9bb1573e8dbf13a28b1e296923006f636cc
Polina Bogdanovich <yapolli10@gmail.com>
2025-10-08 19:25:22 +0300

    refactor code

--- src/main/java/by/dominos/ui/HomePage.java
+++ src/main/java/by/dominos/ui/HomePage.java
@@ -6,6 +6,10 @@ public class HomePage {
     private final String URL = "https://dominos.by/";
     private final String BUTTON_ACCEPT_COOKIES = "//div[@class='CookieButtons']/button[@class='base-tab-button selected']";
     private final String BUTTON_USER_ACCOUNT = "//button[contains(text(), 'Войти')]";
+    private final String BUTTON_MORE = "//label[@for=\"burger\"]//div[@class=\"icon\"]";
+    private final String LINK_DELIVERY_CONDITIONS = "//div[@class='side-menu']//a[@href='https://dominos.by/ru/minsk/delivery_conditions/']";
+    private final String BUTTON_TO_CART = "//button[@class='red-button']//div[contains(text(), 'В корзину')]";
+    private final String LINK_ORDER = "//a[@class='header-cart__order-btn']";
 
     public void openSite() {
         WebDriverSingleton.getDriver().navigate().to(URL);
@@ -17,5 +21,21 @@ public class HomePage {
 
     public void clickUserAccount() {
         WebDriverSingleton.clickElement(BUTTON_USER_ACCOUNT);
+    }
+
+    public void clickMore() {
+        WebDriverSingleton.clickElement(BUTTON_MORE);
+    }
+
+    public void clickDeliveryConditions() {
+        WebDriverSingleton.clickElement(LINK_DELIVERY_CONDITIONS);
+    }
+
+    public void clickToCart() {
+        WebDriverSingleton.clickElement(BUTTON_TO_CART);
+    }
+
+    public void clickOrder() {
+        WebDriverSingleton.clickElement(LINK_ORDER);
     }
 }
