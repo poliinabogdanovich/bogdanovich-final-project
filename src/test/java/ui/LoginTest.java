675afa282a270e35deeb7869bd7a386bd846dfda
Polina Bogdanovich <yapolli10@gmail.com>
2025-10-08 19:24:24 +0300

    refactor code

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -2,21 +2,29 @@ package ui;
 
 import by.dominos.ui.HomePage;
 import by.dominos.ui.LoginPage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LoginTest extends BaseTest {
+    HomePage homePage = new HomePage();
+    LoginPage loginPage = new LoginPage();
+    private static final Logger logger = LogManager.getLogger();
 
     @Test
-    public void login(){
-        HomePage homePage = new HomePage();
+    @DisplayName("Логин с корректным номером телефона")
+    public void test1(){
         homePage.openSite();
         homePage.acceptCookies();
         homePage.clickUserAccount();
 
-        LoginPage loginPage = new LoginPage();
         loginPage.clickForClients();
-        loginPage.sendKeysPhoneNumber("375291234567");
+        String phoneNumber = "375293438185";
+        logger.info("Ввод номера телефона");
+        loginPage.sendKeysPhoneNumber(phoneNumber);
+        loginPage.clickJoin();
+        loginPage.sendKeysCode("839949");
         loginPage.clickJoin();
     }
 }
