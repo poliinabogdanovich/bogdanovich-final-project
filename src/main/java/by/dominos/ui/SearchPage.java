393badfedb8f1a0767c05ff3303306ffcb613eec
Polina Bogdanovich <yapolli10@gmail.com>
2025-10-08 19:26:08 +0300

    add SearchPage.java

---
+++ src/main/java/by/dominos/ui/SearchPage.java
@@ -0,0 +1,21 @@
+package by.dominos.ui;
+
+import by.dominos.singleton.WebDriverSingleton;
+
+public class SearchPage {
+    private final String INPUT_STREET = "//input[@class='rect-input__input with-right-icon']";
+    private final String INPUT_HOUSE_NUMBER = "//input[@class='rect-input__input']";
+    private final String BUTTON_CHECK_DELIVERY_ADDRESS = "//button[@class='pizza-map-check-address__button']";
+
+    public void sendKeysStreet(String street) {
+        WebDriverSingleton.sendKeys(INPUT_STREET, street);
+    }
+
+    public void sendKeysHouseNumber(String houseNumber) {
+        WebDriverSingleton.sendKeys(INPUT_HOUSE_NUMBER, houseNumber);
+    }
+
+    public void clickCheckDeliveryAddress() {
+        WebDriverSingleton.clickElement(BUTTON_CHECK_DELIVERY_ADDRESS);
+    }
+}
