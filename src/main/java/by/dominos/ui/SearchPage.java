ac8b459f317b8194cd3845cc817a23f177265c91
Polina Bogdanovich <yapolli10@gmail.com>
2025-10-11 22:30:06 +0300

    refactor code

--- src/main/java/by/dominos/ui/SearchPage.java
+++ src/main/java/by/dominos/ui/SearchPage.java
@@ -9,6 +9,9 @@ public class SearchPage {
     private final String INPUT_STREET = "//input[@class='rect-input__input with-right-icon']";
     private final String INPUT_HOUSE_NUMBER = "//input[@class='rect-input__input']";
     private final String BUTTON_CHECK_DELIVERY_ADDRESS = "//button[@class='pizza-map-check-address__button']";
+    private final String MESSAGE_ERROR_EMPTY_STREET = "//span[@class='rect-input__error']";
+    private final String MESSAGE_ERROR_EMPTY_HOUSE_NUMBER = "//span[@class='rect-input__error']";
+    private final String MESSAGE_ERROR_NOT_DELIVERY_ZONE = "//p[@class='check-address-modal__text']";
 
     public void sendKeysStreet(String street) {
         WebDriverSingleton.sendKeys(INPUT_STREET, street);
@@ -20,5 +23,17 @@ public class SearchPage {
 
     public void clickCheckDeliveryAddress() {
         WebDriverSingleton.clickElement(BUTTON_CHECK_DELIVERY_ADDRESS);
+    }
+
+    public String getTextEmptyStreet() {
+        return WebDriverSingleton.findElement(MESSAGE_ERROR_EMPTY_STREET).getText();
+    }
+
+    public String getTextEmptyHouseNumber() {
+        return WebDriverSingleton.findElement(MESSAGE_ERROR_EMPTY_HOUSE_NUMBER).getText();
+    }
+
+    public String getTextNotDeliveryZone() {
+        return WebDriverSingleton.findElement(MESSAGE_ERROR_NOT_DELIVERY_ZONE).getText();
     }
 }
