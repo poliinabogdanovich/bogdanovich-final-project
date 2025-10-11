3a1fb1d174ca07a159d11b7eeb81fa550cc0f25e
Polina Bogdanovich <yapolli10@gmail.com>
2025-10-11 21:59:20 +0300

    refactor code

--- src/main/java/by/dominos/ui/LoginPage.java
+++ src/main/java/by/dominos/ui/LoginPage.java
@@ -1,6 +1,7 @@
 package by.dominos.ui;
 
 import by.dominos.singleton.WebDriverSingleton;
+import org.openqa.selenium.By;
 
 public class LoginPage {
     public LoginPage() {
@@ -9,7 +10,9 @@ public class LoginPage {
     private final String BUTTON_FOR_CLIENTS = "//button[@class='red-button auth-modal__button-active']";
     private final String INPUT_PHONE_NUMBER = "//input[@type='tel']";
     private final String BUTTON_JOIN = "//button[@class='red-button auth-modal__submit-button']";
-    private final String INPUT_CODE = "//input[@class='input__component  auth-modal__input']";
+    private final String INPUT_CODE = "//input[@class='input__component  auth-modal__input ']";
+    private final String MESSAGE_INVALID_PHONE_NUMBER = "//div[@class='input input_with-error']//p[@class='input__error-message']";
+    private final String MESSAGE_INVALID_CODE = "//p[@class='input__error-message']";
 
     public void clickForClients() {
         WebDriverSingleton.clickElement(BUTTON_FOR_CLIENTS);
@@ -25,5 +28,13 @@ public class LoginPage {
 
     public void sendKeysCode(String code) {
         WebDriverSingleton.sendKeys(INPUT_CODE, code);
+    }
+
+    public String getTextInvalidPhoneNumber() {
+       return WebDriverSingleton.findElement(MESSAGE_INVALID_PHONE_NUMBER).getText();
+    }
+
+    public String getTextInvalidCode() {
+        return WebDriverSingleton.findElement(MESSAGE_INVALID_CODE).getText();
     }
 }
