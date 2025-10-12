38d39642a20c28052201b03bccbd3322aac8acab
Polina Bogdanovich <yapolli10@gmail.com>
2025-10-13 00:49:51 +0300

    refactor code

--- src/main/java/by/dominos/ui/SearchPage.java
+++ src/main/java/by/dominos/ui/SearchPage.java
@@ -3,14 +3,12 @@ package by.dominos.ui;
 import by.dominos.singleton.WebDriverSingleton;
 
 public class SearchPage {
-    public SearchPage() {}
+    public SearchPage() {
+    }
 
-    // поля: первый label = улица, второй label = номер дома
     private final String INPUT_STREET = "(//label[@class='rect-input']//input)[1]";
     private final String INPUT_HOUSE_NUMBER = "(//label[@class='rect-input']//input)[2]";
     private final String BUTTON_CHECK_DELIVERY_ADDRESS = "//button[@class='pizza-map-check-address__button']";
-
-    // ошибки: первый span = ошибка улицы, второй = ошибка номера дома
     private final String MESSAGE_ERROR_EMPTY_STREET = "(//label[@class='rect-input']//span[@class='rect-input__error'])[1]";
     private final String MESSAGE_ERROR_EMPTY_HOUSE_NUMBER = "(//label[@class='rect-input']//span[@class='rect-input__error'])[2]";
     private final String MESSAGE_ERROR_NOT_DELIVERY_ZONE = "//p[@class='check-address-modal__text']";
