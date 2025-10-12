6dee884d9470521fd5f2c6c1b5dfcb21af4949d4
Polina Bogdanovich <yapolli10@gmail.com>
2025-10-13 00:50:36 +0300

    refactor code

--- src/main/java/by/dominos/ui/LoginPage.java
+++ src/main/java/by/dominos/ui/LoginPage.java
@@ -1,14 +1,14 @@
 package by.dominos.ui;
 
 import by.dominos.singleton.WebDriverSingleton;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class LoginPage {
     private static final Logger logger = LogManager.getLogger();
