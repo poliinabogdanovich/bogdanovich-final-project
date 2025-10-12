dc0fe5b62fe2c12ab3ac537e0231d9cf263efb9b
Polina Bogdanovich <yapolli10@gmail.com>
2025-10-13 00:49:43 +0300

    refactor code

--- src/main/java/by/dominos/singleton/WebDriverSingleton.java
+++ src/main/java/by/dominos/singleton/WebDriverSingleton.java
@@ -1,11 +1,11 @@
 package by.dominos.singleton;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
@@ -13,7 +13,8 @@ import java.util.List;
 public class WebDriverSingleton {
     private static WebDriver driver;
 
-    private WebDriverSingleton() {}
+    private WebDriverSingleton() {
+    }
 
     public static WebDriver getDriver() {
         if (driver == null) {
