a3e985acee21249bc49c7466714767903c57be0e
Polina Bogdanovich <yapolli10@gmail.com>
2025-10-12 22:09:58 +0300

    refactor code

--- src/main/java/by/dominos/ui/SearchPage.java
+++ src/main/java/by/dominos/ui/SearchPage.java
@@ -3,8 +3,7 @@ package by.dominos.ui;
 import by.dominos.singleton.WebDriverSingleton;
 
 public class SearchPage {
-    public SearchPage() {
-    }
+    public SearchPage() {}
 
     private final String INPUT_STREET = "//input[@class='rect-input__input with-right-icon']";
     private final String INPUT_HOUSE_NUMBER = "//input[@class='rect-input__input']";
@@ -21,7 +20,7 @@ public class SearchPage {
         WebDriverSingleton.sendKeys(INPUT_HOUSE_NUMBER, houseNumber);
     }
 
-    public void clickCheckDeliveryAddress() {
+    public void clickDeliveryAddress() {
         WebDriverSingleton.clickElement(BUTTON_CHECK_DELIVERY_ADDRESS);
     }
 
